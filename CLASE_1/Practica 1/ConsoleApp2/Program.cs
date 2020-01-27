b9ca57f457ca82b6183b97435bd87b89e2c665b5
Daniel Olivas <60111295+DanielOIOD@users.noreply.github.com>
2020-01-27 12:28:55 -0700

    Practica 1

---
+++ CLASE_1/Practica 1/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+     
+
+        //variable de la clase
+         static string nombre = "Daniel";
+        static void Main(string[] args)
+        {
+            if( args[0].Length > 0 && args[0] == nombre) //true or false
+            {
+                //Respondamos con el primer argumento y segundo;
+                Console.WriteLine($"Hola {args[0]} ");
+
+            } else
+            {
+                Console.WriteLine("usuario no autorizado");
+            }
+
+
+        }
+    }
+}
