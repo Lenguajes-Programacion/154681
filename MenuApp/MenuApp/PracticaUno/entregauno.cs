a0b146bbe31a5d002c22649732e8e2bfd883e616
Daniel Olivas <60111295+DanielOIOD@users.noreply.github.com>
2020-05-19 20:59:16 -0600

    actualizacion

---
+++ MenuApp/MenuApp/PracticaUno/entregauno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuApp.PracticaUno
+{
+    class entregauno
+    {
+
+        static string nombre = "Daniel";
+        static void Main(string[] args)
+        {
+           
+            if (args[0].Length == 0 || args[0] != nombre) // true o false
+            {
+                Console.WriteLine("Usuario no autorizado");
+            }
+            else
+            {
+                Console.WriteLine($"Hola! Bienvenido {args[0]}");
+            }
+        }
+    }
+}
---
+++ MenuApp/MenuApp/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using MenuApp.PracticaDos;
+using MenuApp.PracticaUno;
+
+namespace MenuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------- \n");
+            Console.WriteLine("CalculatorApp");
+            Console.WriteLine("----------------- \n");
+            bool close_calculator = false;
+            while (!close_calculator)
+            {
+                Console.WriteLine("Calculator App \n");
+                Console.WriteLine("Elige que proyecto deseas ver:");
+                Console.WriteLine("(1) Comprobacion de usuario");
+                Console.WriteLine("(2) Calculadora");
+                string opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        HolaMundo.Comprobacion();
+                        break;
+                    case "2":
+                        Calculadora.Calc();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Valores no validos, ingresar de nuevo...");
+                        Console.WriteLine("\n");
+                        break;
+                }
+            }
+        }
+    }
+    class HolaMundo
+    {
+        //Variable de la clase.
+        static string nombre = "Daniel";
+        public static void Comprobacion()
+        {
+            Console.WriteLine("Usuario:");
+            string usuario = Console.ReadLine();
+            if (usuario.Length > 0 && usuario == nombre) 
+            {
+                Console.WriteLine($"Hola, Bienvenido {usuario}");
+            }
+            else
+            {
+                Console.WriteLine("Usuario no autorizado");
+            }
+        }
+    
+    }
+
+    class Calculadora
+    {
+        public static void Calc()
+        {
+            FuncionesMejoradas MoreOp = new FuncionesMejoradas();
+            memoria memoria = new memoria();
+            Console.WriteLine("----------------- \n");
+            Console.WriteLine("Calculator");
+            Console.WriteLine("----------------- \n");
+            bool close_app = true;
+            int valor1 = 0;
+            int valor2 = 0;
+            int Res = 0;
+            while (close_app)
+            {
+                Console.WriteLine("Elige una opción:");
+                Console.WriteLine("(1) Suma");
+                Console.WriteLine("(2) Resta");
+                Console.WriteLine("(3) Multiplicación");
+                Console.WriteLine("(4) Disión");
+                Console.WriteLine("(5) Memoria");
+                Console.WriteLine("(6) Borrar Memoria");
+                Console.WriteLine("7 Para salir \n");
+                string opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Suma \n");
+                        Console.WriteLine("Captura el primer valor:");
+                        valor1 = Res == 0 ? int.Parse(Console.ReadLine()) : Res;
+                        Console.WriteLine("Captura el segundo valor:");
+                        valor2 = int.Parse(Console.ReadLine());
+                        Res = Operaciones.suma(valor1, valor2);
+                        Console.WriteLine("Tu resultado es: {0} \n", Res);
+                        String o = valor1 + "+" + valor2;
+                        MemoriaData data = new MemoriaData(DateTime.Now.ToString(), o, Res.ToString());
+                        memoria.GuardarMemoria(data);
+                        Res = 0;
+                        break;
+                    case "2":
+                        Console.WriteLine("Resta \n");
+                        Console.WriteLine("Captura el primer valor:");
+                        valor1 = Res == 0 ? int.Parse(Console.ReadLine()) : Res;
+                        Console.WriteLine("Captura el segundo valor:");
+                        valor2 = int.Parse(Console.ReadLine());
+                        Res = Operaciones.resta(valor1, valor2);
+                        Console.WriteLine("Tu resultado es: {0} \n", Res);
+                        Res = 0;
+
+                        break;
+                    case "3":
+                        Console.WriteLine("Multiplicación \n");
+                        Console.WriteLine("Captura el primer valor:");
+                        valor1 = Res == 0 ? int.Parse(Console.ReadLine()) : Res;
+                        Console.WriteLine("Captura el segundo valor:");
+                        valor2 = int.Parse(Console.ReadLine());
+                        Res = Operaciones.multi(valor1, valor2);
+                        Console.WriteLine("Tu resultado es: {0} \n", Res);
+                        Res = 0;
+
+                        break;
+                    case "4":
+                        Console.WriteLine("División \n");
+                        Console.WriteLine("Captura el primer valor:");
+                        valor1 = Res == 0 ? int.Parse(Console.ReadLine()) : Res;
+                        Console.WriteLine("Captura el segundo valor:");
+                        valor2 = int.Parse(Console.ReadLine());
+                        Res = Operaciones.division(valor1, valor2);
+                        Console.WriteLine("Tu resultado es: {0} \n", Res);
+                        Res = 0;
+
+                        break;
+                    case "5":
+                        Console.WriteLine("Accediendo a Memoria \n");
+                        Console.WriteLine("-----------------\n");
+                        //memoria.multidimensional();
+                        String key = Console.ReadLine();
+                        Res = memoria.GetMemoriaData(key);
+                        Console.WriteLine(Res);
+                        break;
+                    case "6":
+                        Console.WriteLine("Memoria Temporal Reiniciada! \n");
+                        Res = 0;
+                        break;
+                    case "7":
+                        Console.WriteLine("-----------------\n");
+                        Console.WriteLine("Cerrando Calculator Nos vemos! \n");
+                        close_app = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opción invalida: {0}, intenta de nuevo \n", opcion);
+                        break;
+                }
+            }
+        }
+    }
+    class Operaciones
+    {
+        public int valor1;
+        public int valor2;
+
+        public static int suma(int valor1, int valor2)
+        {
+            int result = 0;
+            result = valor1 + valor2;
+            return result;
+        }
+        public static int resta(int valor1, int valor2)
+        {
+            int result = 0;
+            result = valor1 - valor2;
+            return result;
+        }
+        public static int multi(int valor1, int valor2)
+        {
+            int result = 0;
+            result = valor1 * valor2;
+            return result;
+        }
+        public static int division(int valor1, int valor2)
+        {
+            int result = 0;
+            result = valor1 / valor2;
+            return result;
+        }
+    }
+
+    class FuncionesMejoradas : Operaciones
+    {
+        public void Potencia(float valor1, float valor2)
+        {
+            double Opecuad = Math.Pow(valor1, valor2);
+            Console.WriteLine("Su numero {0} elevado por {1} es {2}", valor1, valor2, Opecuad);
+        }
+    }
+}
+
